4884930a8ba7e93116053c9e2748faba8fda926c
Giuseppe <giuseppe.concas@gmail.com>
2021-07-05 07:43:59 -0300

    src delted

--- src/app/auth/auth-routes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { LoginComponent } from "./login/login.component";
-
-export const AuthRoutes: Routes = [
-  {
-      path: 'login',
-      component: LoginComponent
-  },
-];
